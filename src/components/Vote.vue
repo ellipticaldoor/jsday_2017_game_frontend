d4fbc2c2a5d937224d83b97ffa86fb491710fa79
Miguel Dorta <dortamiguel@gmail.com>
2017-11-09 23:18:18 +0000

    Added vote buttons

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -1,17 +1,53 @@
 <template>
-	<div class="vote">
-		<h1>{{ msg }}</h1>
+	<div id='vote'>
+
+		<div v-if='status == "choose"'>
+			<button class='blue' @click='choose("blue")'>blue</button>
+			<button class='red' @click='choose("red")'>red</button>
+		</div>
+
+		<div v-else>
+			<button :class='color'>vote</button>
+		</div>
+
 	</div>
 </template>
 
 <script>
+// import { mapActions } from 'vuex'
+
 export default {
 	name: 'vote',
 
 	data() {
 		return {
-			msg: 'Vote',
+			status: 'choose',
+			color: '',
 		}
+	},
+
+	methods: {
+		choose(color) {
+			this.color = color
+			this.status = 'vote'
+		},
 	},
 }
 </script>
+
+<style lang='sass' scoped>
+button
+	border: 0
+	width: 100%
+	font-size: 200%
+	height: 35vh
+
+button:active
+	border: 10px dashed white
+
+.blue
+	background: blue
+
+.red
+	background: red
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import 'milligram'
 
 Vue.config.productionTip = false
 
